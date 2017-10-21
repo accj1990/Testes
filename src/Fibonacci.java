cb84132b26c81189b94fe1e8ea5c8eb0fb6b0fbd
accj1990 <accj1990@gmail.com>
2017-10-21 19:43:58 -0200

    fibo

---
+++ src/Fibonacci.java
@@ -0,0 +1,23 @@
+
+public class Fibonacci 
+{
+	public static void main(String[] args) 
+	{
+		int vet[]= new int[20];
+		vet[0] = 0;
+		vet[1] = 1;
+		
+		for(int i = 2; i < vet.length;i++)
+		{
+			vet[i] = (vet[i-1] + vet[i-2]);
+		}
+		
+		
+		for(int i = 0; i < vet.length;i++)
+		{
+			System.out.println(vet[i]);
+		}
+
+	}
+
+}
