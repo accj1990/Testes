d8d920723610f00e72551a5e535a41802f66cc40
accj1990 <accj1990@gmail.com>
2017-12-30 00:56:56 -0200

    Exemplo de parametro variadico em java

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,16 +3,19 @@ public class Fibonacci
 {
 	public static void main(String[] args) 
 	{
-		int vet[]= new int[20];
-		vet[0] = 0;
-		vet[1] = 1;
+		int vet[]= new int[20]; // vetor para guardar a sequencia
 		
-		for(int i = 2; i < vet.length;i++)
+		/* primeiros elementos */
+		vet[0] = 0;
+		vet[1] = 1;  
+		
+		// calcular a sequencia
+		for(int i = 2; i<vet.length;i++)
 		{
 			vet[i] = (vet[i-1] + vet[i-2]);
 		}
 		
-		
+		// exibir a sequencia
 		for(int i = 0; i < vet.length;i++)
 		{
 			System.out.println(vet[i]);
