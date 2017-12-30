3f9648a701bb1e2b6b0b996e187dd558797c8930
accj1990 <accj1990@gmail.com>
2017-12-30 00:58:05 -0200

    exemplo de parametro variadico java

---
+++ src/ExemploParametroVariadico.java
@@ -0,0 +1,20 @@
+public class ExemploParametroVariadico 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(somar(1,2,3));
+		System.out.println(somar(1,2,3,4));
+
+	}
+	
+	public static int somar (int... numero)
+	{
+		int soma = 0;
+		for(int i = 0; i<numero.length; i++)
+		{
+			soma+=numero[i];
+		}
+		
+		return soma;
+	}
+}
